87c6ca1108848c3f53dc7d518ececb265aeeabf0
David Elmquist <daelmqui@microsoft.com>
2025-05-08 16:50:30 +0200

    changed ion key to env

--- projects/demo/src/environments/environment.ts
+++ projects/demo/src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   server: 'http://localhost:3000',
+  cesiumIonKey: 'IONKEY',
 };
 
 /*
--- projects/demo/src/main.ts
+++ projects/demo/src/main.ts
@@ -8,8 +8,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2YTY1NDYzYS01YzgxLT' +
-  'Q2MGUtODBiYy0zODRmY2MwOGY4MDIiLCJpZCI6MjA1LCJpYXQiOjE1MDQ3MjQ1Njh9.rKgXUKAfFiiSAm_b9T8bpsDVdj0YyZeqGxNpzLlhxpk';
+Ion.defaultAccessToken = environment.cesiumIonKey;
 window['CESIUM_BASE_URL'] = '/assets/Cesium/';
-  platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
